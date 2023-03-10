d175107326d8d81ad43b2caf7e8fee94207eeea1
minguu42 <minguu42@gmail.com>
2023-03-10 20:09:12 +0900

    リポジトリを初期化する

---
+++ Makefile
@@ -0,0 +1,29 @@
+VERSION  := 0.1.0
+REVISION := $(shell git rev-parse --short HEAD)
+LDFLAGS  := "-X main.revision=$(REVISION)"
+
+.DEFAULT_GOAL := help
+.PHONY: setup build run fmt lint test help
+
+setup: ## 開発に必要なツールをインストールする
+	go install golang.org/x/tools/cmd/goimports@latest
+	go install honnef.co/go/tools/cmd/staticcheck@latest
+
+build: ## serverプログラムをビルドする
+	go build -ldflags $(LDFLAGS) -o server ./cmd/server
+
+run: ## serverプログラムを実行する
+	@go run ./cmd/server
+
+fmt: ## フォーマットを実行する
+	@goimports -w .
+
+lint: ## 静的解析を実行する
+	@go vet ./...
+	@staticcheck ./...
+
+test: ## テストを実行する
+	@go test ./...
+
+help: ## ヘルプを表示する
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-10s\033[0m %s\n", $$1, $$2}'
---
+++ cmd/server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {})
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	shutdownErr := make(chan error, 1)
+	go func() {
+		sigterm := make(chan os.Signal, 1)
+		signal.Notify(sigterm, syscall.SIGTERM)
+		<-sigterm
+
+		if err := s.Shutdown(context.Background()); err != nil {
+			shutdownErr <- fmt.Errorf("s.Shutdown failed: %w", err)
+			return
+		}
+		shutdownErr <- nil
+	}()
+
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("s.ListenAndServe failed: %v", err)
+	}
+	if err := <-shutdownErr; err != nil {
+		log.Fatal(err)
+	}
+}
