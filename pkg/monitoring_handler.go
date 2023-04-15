3933be1ee9848072a17b869161455e8eed1a4e68
minguu42 <minguu42@gmail.com>
2023-04-15 18:47:12 +0900

    fix: 加えた変更を戻す

--- pkg/monitoring_handler.go
+++ pkg/monitoring_handler.go
@@ -3,7 +3,5 @@ package pkg
 import "net/http"
 
 func GetHealth() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte{'a', 'b', 'c', '\n'})
-	}
+	return func(w http.ResponseWriter, r *http.Request) {}
 }
