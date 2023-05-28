f80f2dbbc443d27910d833a78310d8c54eb5217c
minguu42 <minguu42@gmail.com>
2023-05-28 17:24:37 +0900

    feat: ogenによる生成コマンドを追加する

---
+++ gen/ogen.go
@@ -0,0 +1,3 @@
+package gen
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest -clean -no-client -no-webhook-client -no-webhook-server -package ogen -target ../pkg/ogen ../doc/openapi.yaml
