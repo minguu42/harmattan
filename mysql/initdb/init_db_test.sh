7fb18aa29113c8cbaccfbaaffda8ffc5827914af
minguu42 <minguu42@gmail.com>
2023-06-17 11:21:44 +0900

    fix: ディレクトリ構成を修正する

--- mysql/initdb/init_db_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_TEST}
-EOF
-
-mysql -u root -D ${MYSQL_DATABASE_TEST} < /docker-entrypoint-initdb.d/schema.sql
