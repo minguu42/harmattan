bea8bd641be86ff6e9323e9853b942644b4be6a8
minguu42 <minguu42@gmail.com>
2023-07-16 08:25:36 +0900

    feat: projectsテーブルにcolor、is_archivedカラムを追加する

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id         CHAR(26)    PRIMARY KEY,
+    id         CHAR(26) PRIMARY KEY,
     name       VARCHAR(15) NOT NULL UNIQUE,
     api_key    CHAR(64)    NOT NULL UNIQUE,
     created_at DATETIME    NOT NULL,
@@ -11,15 +11,17 @@ CREATE TABLE users (
 );
 
 CREATE TABLE projects (
-    id         CHAR(26)    PRIMARY KEY,
-    user_id    CHAR(26)    NOT NULL,
-    name       VARCHAR(20) NOT NULL,
-    created_at DATETIME    NOT NULL,
-    updated_at DATETIME    NOT NULL ON UPDATE CURRENT_TIMESTAMP
+    id          CHAR(26) PRIMARY KEY,
+    user_id     CHAR(26)    NOT NULL,
+    name        VARCHAR(20) NOT NULL,
+    color       CHAR(6)     NOT NULL,
+    is_archived BOOLEAN     NOT NULL,
+    created_at  DATETIME    NOT NULL,
+    updated_at  DATETIME    NOT NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tasks (
-    id           CHAR(26)    PRIMARY KEY,
+    id           CHAR(26) PRIMARY KEY,
     project_id   CHAR(26)    NOT NULL,
     title        VARCHAR(80) NOT NULL,
     completed_at DATETIME,
