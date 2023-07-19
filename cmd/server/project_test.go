a35ebf51f432ab2af3984a94dbfeff5fd4d5c1cf
minguu42 <minguu42@gmail.com>
2023-07-19 22:20:46 +0900

    fix!: 回帰テストの方法を変更する

--- cmd/server/project_test.go
+++ cmd/server/project_test.go
@@ -4,188 +4,46 @@ import (
 	"net/http"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
 	"github.com/minguu42/mtasks/gen/mock"
-	"github.com/minguu42/mtasks/gen/ogen"
 )
 
-func TestCreateProject(t *testing.T) {
-	tests := []test{
+func TestProject(t *testing.T) {
+	run(t, []test{
 		{
-			name: "プロジェクトを作成する",
-			request: request{
-				method: http.MethodPost,
-				path:   "/projects",
-				body:   strings.NewReader(`{"name": "新プロジェクト", "color": "#1A2B3C"}`),
-			},
-			response: response{
-				statusCode: http.StatusCreated,
-				body: ogen.Project{
-					ID:         "01DXF6DT000000000000000002",
-					Name:       "新プロジェクト",
-					Color:      "#1A2B3C",
-					IsArchived: false,
-					CreatedAt:  time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-					UpdatedAt:  time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-				},
+			id:            "createProject",
+			method:        http.MethodPost,
+			path:          "/projects",
+			body:          strings.NewReader(`{"name": "新プロジェクト", "color": "#1A2B3C"}`),
+			statusCode:    http.StatusCreated,
+			needsRollback: true,
+			prepareMockFn: func(t *testing.T) {
+				m := mock.NewMockIDGenerator(gomock.NewController(t))
+				m.EXPECT().Generate().Return("01DXF6DT000000000000000002")
+				tdb.SetIDGenerator(m)
 			},
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			g := mock.NewMockIDGenerator(gomock.NewController(t))
-			g.EXPECT().Generate().Return("01DXF6DT000000000000000002")
-			tdb.SetIDGenerator(g)
-
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			var got ogen.Project
-			resp, err := doTestRequest(tt.request, &got)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-			if diff := cmp.Diff(tt.response.body, got); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestListProject(t *testing.T) {
-	tests := []test{
 		{
-			name: "プロジェクト一覧を取得する",
-			request: request{
-				method: http.MethodGet,
-				path:   "/projects?sort=createdAt",
-			},
-			response: response{
-				statusCode: http.StatusOK,
-				body: ogen.Projects{
-					Projects: []ogen.Project{
-						{
-							ID:         "01DXF6DT000000000000000000",
-							Name:       "プロジェクト1",
-							Color:      "#1A2B3C",
-							IsArchived: false,
-							CreatedAt:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-							UpdatedAt:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-						},
-						{
-							ID:         "01DXF6DT000000000000000001",
-							Name:       "プロジェクト2",
-							Color:      "#1A2B3C",
-							IsArchived: false,
-							CreatedAt:  time.Date(2020, 1, 1, 0, 0, 1, 0, time.UTC),
-							UpdatedAt:  time.Date(2020, 1, 1, 0, 0, 1, 0, time.UTC),
-						},
-					},
-					HasNext: false,
-				},
-			},
+			id:         "listProject",
+			method:     http.MethodGet,
+			path:       "/projects?sort=createdAt",
+			statusCode: http.StatusOK,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var got ogen.Projects
-			resp, err := doTestRequest(tt.request, &got)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-			if diff := cmp.Diff(tt.response.body, got); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestUpdateProject(t *testing.T) {
-	tests := []test{
 		{
-			name: "プロジェクト1を更新する",
-			request: request{
-				method: http.MethodPatch,
-				path:   "/projects/01DXF6DT000000000000000000",
-				body:   strings.NewReader(`{"name": "新プロジェクト1"}`),
-			},
-			response: response{
-				statusCode: http.StatusOK,
-				body: ogen.Project{
-					ID:         "01DXF6DT000000000000000000",
-					Name:       "新プロジェクト1",
-					Color:      "#1A2B3C",
-					IsArchived: false,
-					CreatedAt:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-					UpdatedAt:  time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-				},
-			},
+			id:            "updateProject",
+			method:        http.MethodPatch,
+			path:          "/projects/01DXF6DT000000000000000000",
+			body:          strings.NewReader(`{"name": "新プロジェクト1"}`),
+			statusCode:    http.StatusOK,
+			needsRollback: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			var got ogen.Project
-			resp, err := doTestRequest(tt.request, &got)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-			if diff := cmp.Diff(tt.response.body, got); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestDeleteProject(t *testing.T) {
-	tests := []test{
 		{
-			name: "プロジェクト1を削除する",
-			request: request{
-				method: http.MethodDelete,
-				path:   "/projects/01DXF6DT000000000000000000",
-			},
-			response: response{
-				statusCode: http.StatusNoContent,
-			},
+			id:            "deleteProject",
+			method:        http.MethodDelete,
+			path:          "/projects/01DXF6DT000000000000000000",
+			statusCode:    http.StatusNoContent,
+			needsRollback: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			resp, err := doTestRequest(tt.request, nil)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-		})
-	}
+	})
 }
--- cmd/server/task_test.go
+++ cmd/server/task_test.go
@@ -4,196 +4,46 @@ import (
 	"net/http"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
 	"github.com/minguu42/mtasks/gen/mock"
-	"github.com/minguu42/mtasks/gen/ogen"
 )
 
-func TestCreateTask(t *testing.T) {
-	tests := []test{
+func TestTask(t *testing.T) {
+	run(t, []test{
 		{
-			name: "タスクを作成する",
-			request: request{
-				method: http.MethodPost,
-				path:   "/projects/01DXF6DT000000000000000000/tasks",
-				body:   strings.NewReader(`{"title": "新タスク", "content": "Hello, 世界!", "priority": 3, "dueOn": "2020-01-02"}`),
+			id:         "createTask",
+			method:     http.MethodPost,
+			path:       "/projects/01DXF6DT000000000000000000/tasks",
+			body:       strings.NewReader(`{"title": "新タスク", "content": "Hello, 世界!", "priority": 3, "dueOn": "2020-01-02"}`),
+			statusCode: http.StatusCreated,
+			prepareMockFn: func(t *testing.T) {
+				g := mock.NewMockIDGenerator(gomock.NewController(t))
+				g.EXPECT().Generate().Return("01DXF6DT000000000000000002")
+				tdb.SetIDGenerator(g)
 			},
-			response: response{
-				statusCode: http.StatusCreated,
-				body: ogen.Task{
-					ID:          "01DXF6DT000000000000000002",
-					ProjectID:   "01DXF6DT000000000000000000",
-					Title:       "新タスク",
-					Content:     "Hello, 世界!",
-					Priority:    3,
-					DueOn:       ogen.OptDate{Value: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC), Set: true},
-					CompletedAt: ogen.OptDateTime{Set: false},
-					CreatedAt:   time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-					UpdatedAt:   time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-				},
-			},
+			needsRollback: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			g := mock.NewMockIDGenerator(gomock.NewController(t))
-			g.EXPECT().Generate().Return("01DXF6DT000000000000000002")
-			tdb.SetIDGenerator(g)
-
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			var got ogen.Task
-			resp, err := doTestRequest(tt.request, &got)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-			if diff := cmp.Diff(tt.response.body, got); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestListTask(t *testing.T) {
-	tests := []test{
 		{
-			name: "タスク一覧を取得する",
-			request: request{
-				method: http.MethodGet,
-				path:   "/projects/01DXF6DT000000000000000000/tasks?sort=createdAt",
-			},
-			response: response{
-				statusCode: http.StatusOK,
-				body: ogen.Tasks{
-					Tasks: []ogen.Task{
-						{
-							ID:          "01DXF6DT000000000000000000",
-							ProjectID:   "01DXF6DT000000000000000000",
-							Title:       "タスク1",
-							Content:     "Hello, 世界!",
-							Priority:    0,
-							DueOn:       ogen.OptDate{Set: false},
-							CompletedAt: ogen.OptDateTime{Set: false},
-							CreatedAt:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-							UpdatedAt:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-						},
-						{
-							ID:          "01DXF6DT000000000000000001",
-							ProjectID:   "01DXF6DT000000000000000000",
-							Title:       "タスク2",
-							Content:     "",
-							Priority:    3,
-							DueOn:       ogen.OptDate{Value: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC), Set: true},
-							CompletedAt: ogen.OptDateTime{Value: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC), Set: true},
-							CreatedAt:   time.Date(2020, 1, 1, 0, 0, 1, 0, time.UTC),
-							UpdatedAt:   time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-						},
-					},
-					HasNext: false,
-				},
-			},
+			id:         "listTask",
+			method:     http.MethodGet,
+			path:       "/projects/01DXF6DT000000000000000000/tasks?sort=createdAt",
+			statusCode: http.StatusOK,
 		},
-	}
-	for _, tt := range tests {
-		var got ogen.Tasks
-		resp, err := doTestRequest(tt.request, &got)
-		if err != nil {
-			t.Fatalf("doTestRequest failed: %s", err)
-		}
-
-		if tt.response.statusCode != resp.StatusCode {
-			t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-		}
-		if diff := cmp.Diff(tt.response.body, got); diff != "" {
-			t.Errorf("response body mismatch (-want +got):\n%s", diff)
-		}
-	}
-}
-
-func TestUpdateTask(t *testing.T) {
-	tests := []test{
 		{
-			name: "タスク1を更新する",
-			request: request{
-				method: http.MethodPatch,
-				path:   "/projects/01DXF6DT000000000000000000/tasks/01DXF6DT000000000000000000",
-				body:   strings.NewReader(`{"isCompleted": true}`),
-			},
-			response: response{
-				statusCode: http.StatusOK,
-				body: ogen.Task{
-					ID:          "01DXF6DT000000000000000000",
-					ProjectID:   "01DXF6DT000000000000000000",
-					Title:       "タスク1",
-					Content:     "Hello, 世界!",
-					CompletedAt: ogen.OptDateTime{Value: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC), Set: true},
-					CreatedAt:   time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-					UpdatedAt:   time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
-				},
-			},
+			id:            "updateTask",
+			method:        http.MethodPatch,
+			path:          "/projects/01DXF6DT000000000000000000/tasks/01DXF6DT000000000000000000",
+			body:          strings.NewReader(`{"isCompleted": true}`),
+			statusCode:    http.StatusOK,
+			needsRollback: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			var got ogen.Task
-			resp, err := doTestRequest(tt.request, &got)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-			if diff := cmp.Diff(tt.response.body, got); diff != "" {
-				t.Errorf("response body mismatch (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestDeleteTask(t *testing.T) {
-	tests := []test{
 		{
-			name: "タスク1を削除する",
-			request: request{
-				method: http.MethodDelete,
-				path:   "/projects/01DXF6DT000000000000000000/tasks/01DXF6DT000000000000000000",
-			},
-			response: response{
-				statusCode: http.StatusNoContent,
-			},
+			id:            "deleteTask",
+			method:        http.MethodDelete,
+			path:          "/projects/01DXF6DT000000000000000000/tasks/01DXF6DT000000000000000000",
+			statusCode:    http.StatusNoContent,
+			needsRollback: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tdb.Begin(); err != nil {
-				t.Fatalf("tdb.Begin failed: %s", err)
-			}
-			defer tdb.Rollback()
-
-			resp, err := doTestRequest(tt.request, nil)
-			if err != nil {
-				t.Fatalf("doTestRequest failed: %s", err)
-			}
-
-			if tt.response.statusCode != resp.StatusCode {
-				t.Errorf("status code want %d, but %d", tt.response.statusCode, resp.StatusCode)
-			}
-		})
-	}
+	})
 }
