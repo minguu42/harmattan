3ddf1a0797af2cc60f4180ee05b27afe7e1d80c8
minguu42 <minguu42@gmail.com>
2023-08-05 17:50:52 +0900

    fix: init_db.shを追加する

--- create_test_db.sh
+++
@@ -1,8 +0,0 @@
-# このファイルは以下の想定で作成されている
-# - mysqlコマンドを使用できること
-# - スキーマを作成するSQLファイル/schema.sqlが存在すること
-# - ダミーデータを投入するSQLファイル/import.sqlが存在すること
-
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS opepe_test"
-mysql -u root opepe_test < /schema.sql
-mysql -u root opepe_test < /import.sql
