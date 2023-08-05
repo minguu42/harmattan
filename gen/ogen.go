38e1e7efb9bb99296c2ca854c5c38f817931f78e
minguu42 <minguu42@gmail.com>
2023-08-05 15:23:08 +0900

    fix: ogen.goをgen.goに改名する

--- gen/ogen.go
+++
@@ -1,4 +0,0 @@
-// Package gen は生成コマンドを含むパッケージ
-package gen
-
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest -clean -no-client -no-webhook-client -no-webhook-server -package ogen -target ./ogen ../doc/openapi.yaml
