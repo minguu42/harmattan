54a4917edcc6bd4563f058fa6a17e9e78e6c0305
Akira Furukawa <minguu42@gmail.com>
2025-07-16 20:57:58 +0900

    タスクに関するオペレーションを実装する

--- api/handler/convert.go
+++ api/handler/convert.go
@@ -6,23 +6,44 @@ import (
 	"github.com/minguu42/harmattan/internal/openapi"
 )
 
-func convertOptString(o openapi.OptString) *string {
+func convertOptBool(v openapi.OptBool) *bool {
+	if v.Set {
+		return &v.Value
+	}
+	return nil
+}
+
+func convertOptInt(o openapi.OptInt) *int {
 	if o.Set {
 		return &o.Value
 	}
 	return nil
 }
 
-func convertTimePtr(v *time.Time) openapi.OptDateTime {
+func convertOptString(v openapi.OptString) *string {
+	if v.Set {
+		return &v.Value
+	}
+	return nil
+}
+
+func convertDatePtr(v *time.Time) openapi.OptDate {
+	if v != nil {
+		return openapi.OptDate{Value: *v, Set: true}
+	}
+	return openapi.OptDate{}
+}
+
+func convertOptDateTime(v openapi.OptDateTime) *time.Time {
+	if v.Set {
+		return &v.Value
+	}
+	return nil
+}
+
+func convertDateTimePtr(v *time.Time) openapi.OptDateTime {
 	if v != nil {
 		return openapi.OptDateTime{Value: *v, Set: true}
 	}
 	return openapi.OptDateTime{}
-}
-
-func convertOptBool(o openapi.OptBool) *bool {
-	if o.Set {
-		return &o.Value
-	}
-	return nil
 }
