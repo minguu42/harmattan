c76bb80c3e60edee56864febc462b9267ab3aa32
Akira Furukawa <minguu42@gmail.com>
2025-07-27 22:03:53 +0900

    外部キー制約が効いていないバグを修正する

--- infra/mysql/schema.sql
+++ infra/mysql/schema.sql
@@ -1,55 +1,67 @@
-create table if not exists users (
-    id              char(26) primary key,
-    email           varchar(254) not null unique,
+create table users (
+    id              char(26)     not null primary key,
+    email           varchar(254) not null,
     hashed_password char(60)     not null,
     created_at      datetime     not null default current_timestamp,
-    updated_at      datetime     not null default current_timestamp on update current_timestamp
+    updated_at      datetime     not null default current_timestamp on update current_timestamp,
+    unique (email)
 );
 
-create table if not exists projects (
-    id          char(26) primary key,
-    user_id     char(26)     not null references users (id) on delete cascade,
+create table projects (
+    id          char(26)     not null primary key,
+    user_id     char(26)     not null,
     name        varchar(80)  not null,
-    color       varchar(255) not null check (color in ('blue', 'brown', 'default', 'gray', 'green', 'orange', 'pink', 'purple', 'red', 'yellow')),
+    color       varchar(255) not null,
     is_archived tinyint(1)   not null default 0,
     created_at  datetime     not null default current_timestamp,
-    updated_at  datetime     not null default current_timestamp on update current_timestamp
+    updated_at  datetime     not null default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (id) on delete cascade,
+    check (color in ('blue', 'brown', 'default', 'gray', 'green', 'orange', 'pink', 'purple', 'red',
+                     'yellow'))
 );
 
 create table tasks (
-    id           char(26) primary key,
-    user_id      char(26)            not null references users (id) on delete cascade,
-    project_id   char(26)            not null references projects (id) on delete cascade,
-    name         varchar(100)        not null,
-    content      varchar(300)        not null,
-    priority     tinyint(2) unsigned not null check (priority between 0 and 3),
+    id           char(26)         not null primary key,
+    user_id      char(26)         not null,
+    project_id   char(26)         not null,
+    name         varchar(100)     not null,
+    content      varchar(300)     not null,
+    priority     tinyint unsigned not null,
     due_on       date,
     completed_at datetime,
-    created_at   datetime            not null default current_timestamp,
-    updated_at   datetime            not null default current_timestamp on update current_timestamp
+    created_at   datetime         not null default current_timestamp,
+    updated_at   datetime         not null default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (project_id) references projects (id) on delete cascade,
+    check (priority between 0 and 3)
 );
 
 create table steps (
-    id           char(26) primary key,
-    user_id      char(26)     not null references users (id) on delete cascade,
-    task_id      char(26)     not null references tasks (id) on delete cascade,
+    id           char(26)     not null primary key,
+    user_id      char(26)     not null,
+    task_id      char(26)     not null,
     name         varchar(100) not null,
     completed_at datetime,
-    created_at   datetime    not null default current_timestamp,
-    updated_at   datetime    not null default current_timestamp on update current_timestamp
+    created_at   datetime     not null default current_timestamp,
+    updated_at   datetime     not null default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (task_id) references tasks (id) on delete cascade
 );
 
 create table tags (
-    id         char(26) primary key,
-    user_id    char(26)    not null references users (id) on delete cascade,
+    id         char(26)    not null primary key,
+    user_id    char(26)    not null,
     name       varchar(20) not null,
     created_at datetime    not null default current_timestamp,
-    updated_at datetime    not null default current_timestamp on update current_timestamp
+    updated_at datetime    not null default current_timestamp on update current_timestamp,
+    foreign key (user_id) references users (id) on delete cascade
 );
 
 create table tasks_tags (
     task_id    char(26) not null references tasks (id) on delete cascade,
     tag_id     char(26) not null references tags (id) on delete cascade,
     created_at datetime not null default current_timestamp,
-    primary key (task_id, tag_id)
+    primary key (task_id, tag_id),
+    foreign key (task_id) references tasks (id) on delete cascade,
+    foreign key (tag_id) references tags (id) on delete cascade
 );
