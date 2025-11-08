58035283458b801c0fbd8fba271892be4f0cc460
Akira Furukawa <minguu42@gmail.com>
2025-11-08 20:55:25 +0900

    アクセスログのエラー情報の表示形式を変更する

--- internal/api/usecase/authentication_usecase.go
+++ internal/api/usecase/authentication_usecase.go
@@ -8,6 +8,7 @@ import (
 	"github.com/minguu42/harmattan/internal/auth"
 	"github.com/minguu42/harmattan/internal/database"
 	"github.com/minguu42/harmattan/internal/domain"
+	"github.com/minguu42/harmattan/internal/lib/errtrace"
 	"github.com/minguu42/harmattan/internal/lib/idgen"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -39,22 +40,26 @@ type SignUpOutput struct {
 }
 
 func (uc *Authentication) SignUp(ctx context.Context, in *SignUpInput) (*SignUpOutput, error) {
-	if _, err := uc.DB.GetUserByEmail(ctx, in.Email); !errors.Is(err, database.ErrModelNotFound) {
-		return nil, DuplicateUserEmailError(err)
+	u, err := uc.DB.GetUserByEmail(ctx, in.Email)
+	if err != nil && !errors.Is(err, database.ErrModelNotFound) {
+		return nil, errtrace.Wrap(err)
+	}
+	if u != nil {
+		return nil, errtrace.Wrap(DuplicateUserEmailError())
 	}
 
 	user, err := in.User(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("failed to generate user: %w", err)
+		return nil, errtrace.Wrap(err)
 	}
 
 	token, err := uc.Auth.CreateIDToken(ctx, user.ID)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create id token: %w", err)
+		return nil, errtrace.Wrap(err)
 	}
 
 	if err := uc.DB.CreateUser(ctx, user); err != nil {
-		return nil, fmt.Errorf("failed to create user: %w", err)
+		return nil, errtrace.Wrap(err)
 	}
 	return &SignUpOutput{IDToken: token}, nil
 }
