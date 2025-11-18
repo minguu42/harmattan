9c5561e301cbcafba8fba3abb8830856d80047a3
Akira Furukawa <minguu42@gmail.com>
2025-11-18 09:22:54 +0900

    テストを追加する

--- internal/lib/errtrace/stack_error_test.go
+++ internal/lib/errtrace/stack_error_test.go
@@ -3,6 +3,7 @@ package errtrace_test
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/minguu42/harmattan/internal/lib/errtrace"
@@ -28,4 +29,20 @@ func TestStackError_MarshalJSON(t *testing.T) {
 	require.NoError(t, json.Unmarshal(got, &result))
 	assert.Equal(t, "test error", result["message"])
 	assert.NotEmpty(t, result["frames"])
+}
+
+func TestStackError_Format(t *testing.T) {
+	t.Parallel()
+
+	err := errtrace.Wrap(errors.New("test error"))
+
+	plusVOutput := fmt.Sprintf("%+v", err)
+	assert.Contains(t, plusVOutput, "test error")
+	assert.Contains(t, plusVOutput, "stack_error_test.go")
+
+	vOutput := fmt.Sprintf("%v", err)
+	assert.Equal(t, "test error", vOutput)
+
+	sOutput := fmt.Sprintf("%s", err)
+	assert.Equal(t, "test error", sOutput)
 }
