5cda8a8da0c5d28a27773c83ccc542163d0dc8c9
Akira Furukawa <minguu42@gmail.com>
2025-11-23 10:56:48 +0900

    alog.MaskAttr 関数でエッジケースを考慮する

--- internal/alog/mask_attr.go
+++ internal/alog/mask_attr.go
@@ -5,7 +5,10 @@ import (
 	"reflect"
 )
 
-// MaskAttr masks sensitive fields in log attributes
+// MaskAttr は以下の条件を満たす場合にlogタグの値がmaskに設定された構造体フィールドの値をマスクする
+// - 属性の値の種別が slog.KindAny である
+// - 属性の値は構造体もしくは構造体のポインタである
+// - 構造体のすべてのフィールドがエクスポートされている
 func MaskAttr(a slog.Attr) slog.Attr {
 	if a.Value.Kind() != slog.KindAny {
 		return a
@@ -13,19 +16,41 @@ func MaskAttr(a slog.Attr) slog.Attr {
 
 	switch rv := reflect.ValueOf(a.Value.Any()); rv.Kind() {
 	case reflect.Pointer:
-		if rv.IsNil() || rv.Elem().Kind() != reflect.Struct {
-			return a
-		}
-		// TODO(furukawa): errtrace.StackError でも正しく動くように修正する
-		if rv.Type().Elem().Name() == "StackError" {
+		if rv.IsNil() || rv.Elem().Kind() != reflect.Struct || !allFieldsExported(rv.Elem().Type()) {
 			return a
 		}
 		return slog.Any(a.Key, maskStructValue(rv.Elem()).Addr().Interface())
 	case reflect.Struct:
+		if !allFieldsExported(rv.Type()) {
+			return a
+		}
 		return slog.Any(a.Key, maskStructValue(rv).Interface())
 	default:
 		return a
 	}
+}
+
+func allFieldsExported(t reflect.Type) bool {
+	for i := range t.NumField() {
+		f := t.Field(i)
+		if !f.IsExported() {
+			return false
+		}
+
+		ft := f.Type
+		switch ft.Kind() {
+		case reflect.Pointer:
+			if ft.Elem().Kind() == reflect.Struct && !allFieldsExported(ft.Elem()) {
+				return false
+			}
+		case reflect.Struct:
+			if !allFieldsExported(ft) {
+				return false
+			}
+		default:
+		}
+	}
+	return true
 }
 
 func maskStructValue(v reflect.Value) reflect.Value {
@@ -33,13 +58,7 @@ func maskStructValue(v reflect.Value) reflect.Value {
 	newStruct := reflect.New(t).Elem()
 	for i := range v.NumField() {
 		structField := t.Field(i)
-		if !structField.IsExported() {
-			continue
-		}
 		newField := newStruct.Field(i)
-		if !newField.CanSet() {
-			continue
-		}
 
 		if tag := structField.Tag.Get("log"); tag == "mask" {
 			setMaskedValue(newField)
