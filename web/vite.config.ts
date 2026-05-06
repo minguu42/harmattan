7af992b542cc8a50702d57e7b3e87f5cbe92dbe1
Akira Furukawa <minguu42@gmail.com>
2026-05-06 19:51:49 +0900

    oxfmtの設定変更

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite-plus";
 // https://vite.dev/config/
 export default defineConfig({
   fmt: {
-    ignorePatterns: [],
+    ignorePatterns: ["src/routeTree.gen.ts"],
     sortImports: true,
     sortTailwindcss: {
       functions: ["tv"],
