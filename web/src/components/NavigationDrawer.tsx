2ba6a1d148eb81956ac9b5d49d84e1e4dfbaaacf
Akira Furukawa <50062855+minguu42@users.noreply.github.com>
2026-05-07 09:04:09 +0900

    Inputを実装する (#262)

--- web/src/components/NavigationDrawer.tsx
+++ web/src/components/NavigationDrawer.tsx
@@ -19,16 +19,17 @@ import { type Project } from "../api/projects.ts";
 import { useCreateProject, useDeleteProject, useProjects } from "../api/projects.ts";
 import { Button } from "./Button.tsx";
 import { IconButton } from "./IconButton.tsx";
+import { Input } from "./Input.tsx";
 import { Menu, MenuItem } from "./Menu.tsx";
 
 export function NavigationDrawer() {
   return (
-    <div className="min-h-svh w-280 border-r border-border bg-surface">
+    <div className="min-h-svh w-280 border-r border-border bg-sidebar">
       <div className="flex h-56 items-center gap-8 px-12">
         <Avatar.Root className="inline-flex size-32 items-center justify-center overflow-hidden rounded-full bg-red-600 align-middle text-base text-white select-none">
           YF
         </Avatar.Root>
-        <div className="text-sm font-semibold text-on-surface">ボッスン</div>
+        <div className="text-sm font-semibold text-on-sidebar">ボッスン</div>
       </div>
       <ul className="mt-8">
         <Indicator icon={SunIcon} label="今日" />
@@ -38,13 +39,13 @@ export function NavigationDrawer() {
       </ul>
       <ProjectIndicatorList />
       <div className="mt-16 flex h-32 items-center justify-between pr-16 pl-12">
-        <div className="text-sm font-semibold">タグ</div>
+        <div className="text-sm font-semibold text-on-sidebar">タグ</div>
         <IconButton icon={PlusIcon} size="sm" />
       </div>
       <ul>
         <li className="flex h-36 items-center gap-8 px-16">
           <TagIcon />
-          <div className="text-sm">タグ1</div>
+          <div className="text-sm text-on-sidebar">タグ1</div>
         </li>
       </ul>
     </div>
@@ -56,7 +57,7 @@ function Indicator({ icon: Icon, label }: { icon: LucideIcon; label: string }) {
     <li>
       <Link
         to="/"
-        className="state-layer mx-8 flex h-36 items-center gap-8 rounded-lg px-8 text-sm text-on-surface"
+        className="state-layer mx-8 flex h-36 items-center gap-8 rounded-lg px-8 text-sm text-on-sidebar"
       >
         <Icon className="size-24" />
         {label}
@@ -77,8 +78,8 @@ function ProjectIndicatorList() {
   return (
     <>
       <div className="mt-16 flex h-32 items-center justify-between pr-16 pl-12">
-        <div className="text-sm font-semibold">プロジェクト</div>
-        <ProjectCreateDialog />
+        <div className="text-sm font-semibold text-on-sidebar">プロジェクト</div>
+        <ProjectCreateButtonWithDialog />
       </div>
       <ul>
         {projects.map((p) => (
@@ -89,7 +90,7 @@ function ProjectIndicatorList() {
   );
 }
 
-function ProjectCreateDialog() {
+function ProjectCreateButtonWithDialog() {
   const [open, setOpen] = useState(false);
 
   return (
@@ -97,15 +98,16 @@ function ProjectCreateDialog() {
       <IconButton icon={PlusIcon} size="sm" onClick={() => setOpen(true)} />
       <Dialog.Portal>
         <Dialog.Backdrop forceRender className="fixed inset-0 bg-scrim" />
-        <ProjectCreatePopup closePopup={() => setOpen(false)} />
+        <ProjectCreateDialog closePopup={() => setOpen(false)} />
       </Dialog.Portal>
     </Dialog.Root>
   );
 }
 
-function ProjectCreatePopup({ closePopup }: { closePopup: () => void }) {
+function ProjectCreateDialog({ closePopup }: { closePopup: () => void }) {
   const [name, setName] = useState("");
   const createProject = useCreateProject();
+
   function addProject() {
     if (name.trim() === "") {
       return;
@@ -120,17 +122,20 @@ function ProjectCreatePopup({ closePopup }: { closePopup: () => void }) {
   }
 
   return (
-    <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-32 w-400 -translate-x-1/2 -translate-y-1/2 rounded-xl border border-border bg-surface p-24">
-      <Dialog.Title className="text-2xl text-on-surface">プロジェクト作成</Dialog.Title>
+    <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-32 w-400 -translate-x-1/2 -translate-y-1/2 rounded-xl border border-border bg-background p-16">
+      <Dialog.Title className="text-base font-medium text-on-surface">
+        プロジェクト作成
+      </Dialog.Title>
       <Form
         onSubmit={(e) => {
           e.preventDefault();
           addProject();
         }}
       >
-        <Field.Root>
-          <Field.Control
-            className="h-56 w-352 px-16 text-on-surface placeholder:text-on-surface-variant"
+        <Field.Root className="my-16 flex flex-col gap-4">
+          <Field.Label className="text-sm font-medium text-on-background">プロジェクト名</Field.Label>
+          <Input
+            type="text"
             required
             placeholder="プロジェクト名"
             value={name}
